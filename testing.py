ff32af4751a1c37763de6f8feb77d9fd5cff5aaa
Czerwonogrodzki <czerwonogrodzkipatryk@gmail.com>
2023-06-23 17:31:45 +0200

    fixed?

--- testing.py
+++ testing.py
@@ -13,14 +13,24 @@ def bubble_sort(arr):
     return array
 
 
+def single_process_sort_time(data_array):
+    try:
+        start_time = time.time()
+        bubble_sort(data_array)
+        single_process_time = time.time() - start_time
+
+        return single_process_time
+    except:
+        return -1
+
+
 def parallel_sort(arr, num_processes):
     chunk_size = len(arr) // num_processes
     chunks = [arr[i:i + chunk_size] for i in range(0, len(arr), chunk_size)]
 
     sorted_chunks = bubble_sort(chunks)
 
-    sorted_arr = sorted(sum(sorted_chunks, []))
-    #return sorted_arr
+    sorted(sum(sorted_chunks, []))
 
 
 # Przykładowe dane do posortowania
@@ -29,17 +39,6 @@ data_size = 10000
 
 def create_data_array(data_size):
     return [random.randint(1, 100) for _ in range(data_size)]
-
-
-def single_process_sort_time(data_array):
-    try:
-        start_time = time.time()
-        sorted_data_single = bubble_sort(data_array)
-        single_process_time = time.time() - start_time
-        del sorted_data_single
-        return single_process_time
-    except:
-        return -1
 
 
 def multiprocess_parrallel_sort_time(data_array, num_processes):
@@ -67,25 +66,28 @@ def multiprocess_parrallel_sort_time(data_array, num_processes):
 def comparing_sort_time(max_data_size):
     max_num_processes = multiprocessing.cpu_count()
     best_function = ''
-    best_time = 0
+    best_time = 100
     best_amount_of_processes = 0
     for x in range(1024, max_data_size, 1024):
-        print(f"size: {x}")
+        #print(f"size: {x}")
         data_array = create_data_array(x)
         for y in range(2, max_num_processes):
             single_process_time = single_process_sort_time(data_array)
-            print(f"\nsingle {single_process_time}")
+            #print(f"\nsingle {single_process_time}")
             multiprocess_sort_time = multiprocess_parrallel_sort_time(data_array, y)
-            print(f"multi {multiprocess_sort_time}")
-            # if (single_process_time < multiprocess_sort_time):
-            # if single_process_time < best_time:
-            #     best_function = 'single_process_sort_time'
-            #     best_time = single_process_time
-            #     best_amount_of_processes = 1
-            # elif (multiprocess_sort_time < single_process_time):
-            #     # if multiprocess_sort_time < best_time:
-            #     best_function = 'multiprocess_parrallel_sort_time'
-            #     best_time = multiprocess_sort_time
-            #     best_amount_of_processes = y
-        # print(
-        # f"The best function is {best_function} with a time of {best_time} seconds for data size {len(data_array)} and {best_amount_of_processes} processes.")
+            #print(f"multi {multiprocess_sort_time}")
+            if (single_process_time < multiprocess_sort_time):
+                #if single_process_time < best_time:
+                best_function = 'single_process_sort_time'
+                best_time = single_process_time
+                best_amount_of_processes = 1
+            elif (multiprocess_sort_time < single_process_time):
+                #if multiprocess_sort_time < best_time:
+                best_function = 'multiprocess_parrallel_sort_time'
+                best_time = multiprocess_sort_time
+                best_amount_of_processes = y
+        print(
+        f"The best function is {best_function} with a time of {best_time} seconds for data size {len(data_array)} and {best_amount_of_processes} processes.")
+        best_function = ''
+        best_time = 100
+        best_amount_of_processes = 0
